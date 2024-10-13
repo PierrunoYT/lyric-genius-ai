fc90ba00e1eae560b07b9a74f339f8140ff6b851
“PierrunoYT” <pierrebruno@hotmail.ch>
2024-10-13 14:42:14 +0200

    Add: Genius redirect URI to config

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,13 @@ dotenv.config();
 interface Config {
   geniusAccessToken: string | undefined;
   openrouterApiKey: string | undefined;
+  geniusRedirectUri: string;
 }
 
 const config: Config = {
   geniusAccessToken: process.env.GENIUS_ACCESS_TOKEN,
-  openrouterApiKey: process.env.OPENROUTER_API_KEY
+  openrouterApiKey: process.env.OPENROUTER_API_KEY,
+  geniusRedirectUri: 'http://localhost:3000/callback'
 };
 
 export default config;
